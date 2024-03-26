7bb75be100c5c984fd2561532ce9f116ce41cedb
Guillaume <guillaume.thouvenin@vates.tech>
2024-03-26 20:48:56 +0100

    format & draw a rectangle

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,18 @@
 let setup () =
-        Raylib.init_window 800 450 "raylib example";
-        Raylib.set_target_fps 60
+  Raylib.init_window 800 450 "raylib example";
+  Raylib.set_target_fps 60
 
 let rec loop () =
-        match Raylib.window_should_close () with
-        | true -> Raylib.close_window ()
-        | false ->
-            let open Raylib in
-            begin_drawing ();
-            clear_background Color.raywhite;
-            draw_text "Congrats! You created your first window!" 190 200 20 Color.lightgray;
-            end_drawing ();
-            loop ()
+  match Raylib.window_should_close () with
+  | true -> Raylib.close_window ()
+  | false ->
+      let open Raylib in
+      begin_drawing ();
+      clear_background Color.raywhite;
+      draw_text "Congrats! You created your first window!" 190 200 20
+        Color.lightgray;
+      draw_rectangle 10 10 64 64 Color.red;
+      end_drawing ();
+      loop ()
 
 let () = setup () |> loop
