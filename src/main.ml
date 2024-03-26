451c9694da43ea674ae9f22b5b128f071596bdad
Guillaume <guillaume.thouvenin@vates.tech>
2024-03-26 21:09:16 +0100

    add player and move it

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,13 @@
+type player = { pos_x : int; pos_y : int; width : int; height : int }
+
+let update_player (p : player) (x : int) (y : int) =
+  { p with pos_x = p.pos_x + x; pos_y = p.pos_y + y }
+
 let setup () =
   Raylib.init_window 800 450 "raylib example";
   Raylib.set_target_fps 60
 
-let rec loop () =
+let rec loop p () =
   match Raylib.window_should_close () with
   | true -> Raylib.close_window ()
   | false ->
@@ -11,8 +16,11 @@ let rec loop () =
       clear_background Color.raywhite;
       draw_text "Congrats! You created your first window!" 190 200 20
         Color.lightgray;
-      draw_rectangle 10 10 64 64 Color.red;
+      draw_rectangle p.pos_x p.pos_y p.width p.height Color.red;
       end_drawing ();
-      loop ()
+      (* just add 1 to x and y for testing... *)
+      loop (update_player p 1 1) ()
 
-let () = setup () |> loop
+let () =
+  let p : player = { pos_x = 10; pos_y = 10; width = 10; height = 10 } in
+  setup () |> loop p
