1ab6ac1bd15d07722c7ab9b420d906bb6a3d8d63
Guillaume <guillaume.thouvenin@vates.tech>
2024-03-27 18:45:08 +0100

    position the player almost at center and accelerate...

--- src/main.ml
+++ src/main.ml
@@ -3,11 +3,14 @@ let setup () =
   let width, height = (800, 450) in
   let s : State.t =
     {
-      player = { pos_x = 700; pos_y = 10; width = 10; height = 10 };
+      (* it is the upper left corner that is center... *)
+      player =
+        { pos_x = width / 2; pos_y = height / 2; width = 64; height = 64 };
       window_width = width;
       window_height = height;
       background = Raylib.Color.create 0x68 0x83 0xf5 0xff;
       foreground = Raylib.Color.create 0xf5 0x68 0x83 0xff;
+      acceleration = 100.0;
     }
   in
   Raylib.init_window width height "raylib example";
@@ -19,16 +22,24 @@ let rec loop (s : State.t) =
   | true -> Raylib.close_window ()
   | false ->
       let open Raylib in
+      let rate : float = get_frame_time () in
+      let rate = rate *. s.acceleration in
+
       (* Update the state *)
       let delta_y =
-        if is_key_down Key.Down then 1 else if is_key_down Key.Up then -1 else 0
+        if is_key_down Key.Down then rate
+        else if is_key_down Key.Up then -1.0 *. rate
+        else 0.0
       in
       let delta_x =
-        if is_key_down Key.Right then 1
-        else if is_key_down Key.Left then -1
-        else 0
+        if is_key_down Key.Right then rate
+        else if is_key_down Key.Left then -1.0 *. rate
+        else 0.0
       in
-      let s' = State.update_player s delta_x delta_y in
+      let s' =
+        State.update_player s (int_of_float delta_x) (int_of_float delta_y)
+      in
+      let s' = State.increment_acceleration s' 1.0 in
 
       (* Drawing *)
       begin_drawing ();
--- src/state.ml
+++ src/state.ml
@@ -4,6 +4,7 @@ type t = {
   window_height : int;
   background : Raylib.Color.t;
   foreground : Raylib.Color.t;
+  acceleration : float;
 }
 
 let update_player (s : t) dx dy =
@@ -11,3 +12,13 @@ let update_player (s : t) dx dy =
     Player.update_player s.player dx dy s.window_width s.window_height
   in
   { s with player = new_p }
+
+let increment_acceleration (s : t) v =
+  (* we limite the acceleration to 1000.0 *)
+  let max_acc = 1000.0 in
+  let new_acceleration = s.acceleration +. v in
+  {
+    s with
+    acceleration =
+      (if new_acceleration > max_acc then max_acc else new_acceleration);
+  }
