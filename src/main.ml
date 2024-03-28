2bb1a04cf876a1fe218f7c2ff2931ad1a87c18f5
Guillaume <guillaume.thouvenin@vates.tech>
2024-03-28 16:24:51 +0100

    refactoring update and draw

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,4 @@
-(* Setup and return the current state *)
+(** [setup] sets state [s], init the window and return [s]. *)
 let setup () =
   let width, height = (800, 450) in
   let s : State.t =
@@ -17,36 +17,41 @@ let setup () =
   Raylib.set_target_fps 60;
   s
 
+(** [update state] updates and returns a new state. *)
+let update (s : State.t) =
+  let open Raylib in
+  let frate : float = get_frame_time () in
+  let velocity = frate *. s.acceleration in
+  let delta_y =
+    if is_key_down Key.Down then velocity
+    else if is_key_down Key.Up then -1.0 *. velocity
+    else 0.0
+  in
+  let delta_x =
+    if is_key_down Key.Right then velocity
+    else if is_key_down Key.Left then -1.0 *. velocity
+    else 0.0
+  in
+  let s' =
+    State.update_player s (int_of_float delta_x) (int_of_float delta_y)
+  in
+  State.increment_acceleration s' 1.0
+
+(** [draw state] draws the scene and return [state]. *)
+let draw (s : State.t) =
+  let open Raylib in
+  begin_drawing ();
+  clear_background s.background;
+  draw_rectangle s.player.pos_x s.player.pos_y s.player.width s.player.height
+    s.foreground;
+  end_drawing ();
+  s (* return it so it can be chained beautifully with loop *)
+
+(** [loop state] is the main loop that updates and draws scene. *)
 let rec loop (s : State.t) =
   let open Raylib in
   match window_should_close () with
   | true -> close_window ()
-  | false ->
-      let frate : float = get_frame_time () in
-      let velocity = frate *. s.acceleration in
-
-      (* Update the state *)
-      let delta_y =
-        if is_key_down Key.Down then velocity
-        else if is_key_down Key.Up then -1.0 *. velocity
-        else 0.0
-      in
-      let delta_x =
-        if is_key_down Key.Right then velocity
-        else if is_key_down Key.Left then -1.0 *. velocity
-        else 0.0
-      in
-      let s' =
-        State.update_player s (int_of_float delta_x) (int_of_float delta_y)
-      in
-      let s' = State.increment_acceleration s' 1.0 in
-
-      (* Drawing *)
-      begin_drawing ();
-      clear_background s'.background;
-      draw_rectangle s'.player.pos_x s'.player.pos_y s'.player.width
-        s'.player.height s'.foreground;
-      end_drawing ();
-      loop s'
+  | false -> update s |> draw |> loop
 
 let () = setup () |> loop
